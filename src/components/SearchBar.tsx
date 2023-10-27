95cc260e71d22b5b05411541b9809497351d42e8
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-26 23:14:59 -0400

    create search component

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,27 @@
+// src/components/SearchBar.tsx
+import React, { useState } from 'react';
+
+const SearchBar: React.FC<{ onSearch: (query: string) => void }> = ({
+  onSearch,
+}) => {
+  const [query, setQuery] = useState('');
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    onSearch(query);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Search for a movie..."
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+};
+
+export default SearchBar;
