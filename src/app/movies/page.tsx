7db2be62457cee337b26ca12ad7122be7a76da58
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-11-02 09:26:46 -0400

    Put a section that renders when a search is not happening

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -5,6 +5,7 @@ import React, { useState } from 'react';
 import Link from 'next/link';
 import SearchBar from '../../components/SearchBar';
 import Image from 'next/image';
+
 interface MovieItem {
   title: string;
   year: string;
@@ -35,35 +36,44 @@ const MoviesPage: React.FC = () => {
   };
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Movie List</h1>
+    <div className="container mx-auto p-4 mb-4  bg-black mt-4">
+      <h1 className="text-2xl font-bold mb-4 m-4">Flix Finder App</h1>
       <SearchBar onSearch={handleSearch} />
-      <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-4 text-black max-h-0">
-        {movies.map((movie) => (
-          <li key={movie.imdbID} className="bg-secondary p-4 rounded shadow justify-center">
-            <Link href={`/movies/${movie.imdbID}`}>
-              <div className="block">
-                {movie.image ? (
-                  <Image
-                    src={movie.image.url}
-                    alt={`${movie.title} Poster`}
-                    width={300}
-                    height={350}
-                    className="object-cover mb-2 rounded drop-shadow-lg w-full h-auto"
-                  />
-                ) : (
-                  <div className="w-full h-48 mb-2  flex items-center justify-center rounded bg-black text-headline">
-                    No image available
-                  </div>
-                )}
-                <h2 className="text-lg font-semibold text-center">
-                  {movie.title} ({movie.year})
-                </h2>
-              </div>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      {movies.length === 0 ? (
+        <div className="text-center mt-4">
+          <h2 className="text-xl">Enter a search term to find movies</h2>
+        </div>
+      ) : (
+        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4  text-black max-h-0 mt-8">
+          {movies.map((movie) => (
+            <li
+              key={movie.imdbID}
+              className="bg-secondary p-4 rounded shadow justify-center"
+            >
+              <Link href={`/movies/${movie.imdbID}`}>
+                <div className="block">
+                  {movie.image ? (
+                    <Image
+                      src={movie.image.url}
+                      alt={`${movie.title} Poster`}
+                      width={300}
+                      height={350}
+                      className="object-cover mb-2 rounded drop-shadow-xl w-full h-auto"
+                    />
+                  ) : (
+                    <div className="w-full h-48 mb-2 flex items-center justify-center rounded bg-black text-headline">
+                      No image available
+                    </div>
+                  )}
+                  <h2 className="text-lg font-semibold text-center">
+                    {movie.title} ({movie.year})
+                  </h2>
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
