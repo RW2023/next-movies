2cb1183efc8ce183d476b148e8696d134060c8cd
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-11-02 05:32:04 -0400

    edit api to handle no query params provided

--- src/pages/api/movies.ts
+++ src/pages/api/movies.ts
@@ -1,51 +1,41 @@
+// src/pages/api/movies.ts
+
 import axios from 'axios';
 import { NextApiRequest, NextApiResponse } from 'next';
-
-// Define an interface for individual movie items
-interface MovieItem {
-  Title: string;
-  Year: string;
-  imdbID: string;
-  Type: string;
-  Poster: string;
-}
-
-// Define an interface for the entire API response
-interface ApiResponse {
-  Search: MovieItem[];
-  totalResults: string;
-  Response: string;
-}
-
-// Variables to hold cache data and expiry timestamp
-let cache: ApiResponse | null = null;
-let cacheExpiry: number | null = null;
 
 const handleMoviesRequest = async (
   req: NextApiRequest,
   res: NextApiResponse,
 ) => {
-  if (cache && cacheExpiry && cacheExpiry > Date.now()) {
-    // Cache is still fresh, return cached data
-    return res.json(cache);
+  if (!req.query) {
+    return res.status(400).json({ error: 'No query parameters provided' });
+  }
+
+  const { query } = req.query;
+
+  if (!query) {
+    return res.status(400).json({ error: 'Query parameter is required' });
   }
 
   try {
-    // Fetch data from the external API
     const response = await axios.get(
-      'https://movie-database-alternative.p.rapidapi.com/',
+      'https://imdb8.p.rapidapi.com/title/v2/find',
       {
-        params: { s: 'Avengers Endgame', r: 'json', page: '1' },
+        params: { title: query, limit: '20', sortArg: 'moviemeter,asc' },
         headers: {
-          'X-RapidAPI-Key': process.env.RAPIDAPI_KEY as string, // Use environment variable for API key
-          'X-RapidAPI-Host': 'movie-database-alternative.p.rapidapi.com',
+          'X-RapidAPI-Key': process.env.RAPIDAPI_KEY as string,
+          'X-RapidAPI-Host': 'imdb8.p.rapidapi.com',
         },
       },
     );
-    cache = response.data; // Update cache
-    cacheExpiry = Date.now() + 3600000; // Set cache expiry to 1 hour from now
-    res.json(response.data); // Send the fetched data to the client
+
+    if (response.data) {
+      res.json(response.data);
+    } else {
+      res.status(500).json({ error: 'Failed to fetch data' });
+    }
   } catch (error) {
+    console.error(error); // Log the error object to see more details
     res.status(500).json({ error: 'Failed to fetch data' });
   }
 };
