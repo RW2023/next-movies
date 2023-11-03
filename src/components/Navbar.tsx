708b865c72f8dcf39b5c9add966b011ead9c1099
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-11-03 06:33:49 -0400

    update text size

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,12 +7,12 @@ const Navbar: FC<Props> = (props): JSX.Element => {
   return (
     <div className="navbar bg-tertiary border fixed top-0 w-full z-50">
       <div className="flex">
-        <Link href={'/'} className="btn btn-ghost normal-case text-xl">
+        <Link href={'/'} className="btn btn-ghost normal-case text-2xl">
           Flix Finder
         </Link>
       </div>
       <div className="flex-none">
-        <button type="submit" className="btn btn-square btn-ghost">
+        <button type="submit" className="btn btn-square btn-ghost text-xl">
           <Link href="/movies">Movies</Link>
         </button>
       </div>
