6fd5fb8c8e518f58143a968a9cd635dbdb59ad4b
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-11-03 09:00:48 -0400

    linkify title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className="container mx-auto mt-6 p-4">
       <div className="text-3xl text-center text-headline mb-6 mt-8">
-        <h1 className="p-2 m-1">Search For Movies</h1>
+        <Link href="/movies" className="p-2 m-1">Search For Movies</Link>
       </div>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
         <div className="flex flex-col justify-center items-center bg-background p-4 rounded-lg shadow-lg">
-          <h2 className="mb-4">Search your favorite actor</h2>
+          <h2 className="mb-4 text-xl">Search your favorite actor</h2>
           <Image
             src="/freeman.jpg"
             alt="morgan freeman"
@@ -18,7 +19,7 @@ export default function Home() {
           />
         </div>
         <div className="flex flex-col justify-center items-center bg-background p-4 rounded-lg shadow-lg">
-          <h2 className="mb-4">Search your favorite movie</h2>
+          <h2 className="mb-4 text-xl">Search your favorite movie</h2>
           <Image
             src="/darkNight.png"
             alt="Dark Night"
