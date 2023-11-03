b68b99d53dc55dcb85a36751ce322756785b1839
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-11-02 23:11:27 -0400

    add spacing to the top of the search container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <div>
           <h1 className="text-3xl text-center text-headline">Flix Finder</h1>
         </div>
-        <div className="container flex flex-col align-baseline h-screen grid-cols-4 w-full" data-theme='black'>
+        <div className="container flex flex-col align-baseline h-screen grid-cols-4 w-full mt-6" data-theme='black'>
         </div>
       </>
   
