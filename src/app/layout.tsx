50172ef3c1081eede1b4444a50a4ff1fcbbe7b86
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-11-14 12:04:17 -0500

    adjust margin and hight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,26 @@
-import type { Metadata } from 'next'
+import type { Metadata } from 'next';
 
-import './globals.css'
-import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
-
-
+import './globals.css';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
 export const metadata: Metadata = {
   title: 'Flix Finder',
   description: 'Movie search app',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en" data-theme='dark'>
-      <body className='mt-16'>
-        <Navbar/>
+    <html lang="en" data-theme="dark">
+      <body className="mt-3 h-full">
+      <Navbar />
         {children}
         </body>
-        <Footer/>
+      <Footer />
     </html>
-  )
+  );
 }
