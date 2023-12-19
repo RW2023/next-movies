532b6269b4992638fbe926b2f8426a49ce77c473
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-19 16:59:07 -0500

    Update Loading component styling to fix hight issue

--- src/components/ui/Loading.tsx
+++ src/components/ui/Loading.tsx
@@ -5,7 +5,7 @@ interface Props {}
 const Loading: FC<Props> = (props): JSX.Element => {
   return (
     <div
-      className="h-screen flex items-center justify-center text-center"
+      className="h-auto flex items-center justify-center text-center"
       style={{ height: '75vh' }}
     >
       <p className="text-lg md:text-xl lg:text-2xl sm:-mx-4">
