e5368c01d2b60093503194e913e0260b9b64b5d8
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-19 17:38:38 -0500

    Update data-theme value in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" data-theme="dark">
+    <html lang="en" data-theme="business">
       <head>
         <link
           rel="stylesheet"
