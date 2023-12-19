63889de9438ad536e706c6df240f61e6de5f1744
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-19 10:50:18 -0500

    Add about page component

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Heading from '@/components/ui/Heading'
+
+export default function page() {
+  return (
+    <div className='min-h-screen'>
+   <Heading title="About" iconClass='fas fa-info' />
+    </div>
+  )
+}
