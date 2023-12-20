87df4e372741a4c42f9516ebf4dc7e90830993fc
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-19 19:46:20 -0500

    Update data-theme value in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" data-theme="business">
+    <html lang="en" data-theme="halloween">
       <head>
         <link
           rel="stylesheet"
