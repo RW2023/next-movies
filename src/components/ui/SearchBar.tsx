176c39fd52a620962abb1ea30fbf5a1f3917483b
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-20 11:41:30 -0500

    Update SearchBar component styling

--- src/components/ui/SearchBar.tsx
+++ src/components/ui/SearchBar.tsx
@@ -13,7 +13,7 @@ const SearchBar: React.FC<{ onSearch: (query: string) => void }> = ({
   };
 
   return (
-    <form onSubmit={handleSubmit} className="flex items-center justify-center ">
+    <form onSubmit={handleSubmit} className="flex items-center justify-center align-middle ">
       <input
         className="border-2  rounded-lg p-2 bg-tertiary text-highlight text-lg sm:p-1 w-3/4 lg:w-1/2 my-3"
         type="text"
