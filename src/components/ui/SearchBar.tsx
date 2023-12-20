0882fc5b5942ebc31cd35ceaaf58a722e33dc2ab
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-19 20:21:17 -0500

    Update search bar placeholder

--- src/components/ui/SearchBar.tsx
+++ src/components/ui/SearchBar.tsx
@@ -13,13 +13,13 @@ const SearchBar: React.FC<{ onSearch: (query: string) => void }> = ({
   };
 
   return (
-    <form onSubmit={handleSubmit} className="flex items-center justify-center">
+    <form onSubmit={handleSubmit} className="flex items-center justify-center ">
       <input
         className="border-2 border-headline rounded-lg p-2 bg-tertiary text-highlight text-lg sm:p-1 w-3/4 lg:w-1/2 my-3"
         type="text"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        placeholder="Search for a movie, actor ..."
+        placeholder="Search ..."
       />
       <button type="submit" className="btn btn-primary m-3 border border-base-200">
         Search
