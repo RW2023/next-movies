82b212a22de79c79e7f77a43f1d4302a0e28a1b6
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-20 03:18:51 -0500

    Refactor page layout and add links to movie search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,40 +6,43 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className="container mx-auto mt-1 p-4">
-      <div className="text-3xl text-center mb-2 mt-3">
-        <Link href={'/movies'}>
-          <Heading title="Find FLix" />
-        </Link>
+      <div className="text-3xl text-center mb-2 mt-3 flex">
+        <Link href={'/movies'}></Link>
       </div>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-        <div className="flex flex-col justify-center items-center bg-base-300 p-4 rounded-lg shadow-lg card-title">
-          <SubHeading
-            title="Search your favorite actor"
-            iconClass="fas fa-search"
-          />{' '}
-          <div className='card-body'>
+        <div className="flex flex-col justify-center items-center bg-base-300 p-4 rounded-lg shadow-lg card-title hover:underline">
+          <Link href={'/movies'}>
+            <SubHeading
+              title="Search your favorite actor"
+              iconClass="fas fa-search"
+            />{' '}
+          </Link>
+          <div className="card-body">
             <Image
               src="/freeman.jpg"
               alt="morgan freeman"
-              width={500}
-              height={500}
+              width={200}
+              height={200}
               className="object-cover rounded-lg shadow-lg bg-base-300  p-1 border-2 border-base"
             />
           </div>
         </div>
         <div className="flex flex-col justify-center items-center bg-base-300 p-4 rounded-lg shadow-lg card">
-          <div className="card-title">
-            <SubHeading
-              title="Search your favorite movie"
-              iconClass="fas fa-search"
-            />
+          <div className="card-title hover:underline">
+            {' '}
+            <Link href={'/movies'}>
+              <SubHeading
+                title="Search your favorite movie"
+                iconClass="fas fa-search "
+              />
+            </Link>
           </div>
           <div className="card-body">
             <Image
               src="/darkNight.png"
               alt="Dark Night"
-              width={500}
-              height={450}
+              width={200}
+              height={200}
               className="object-cover rounded-lg shadow-lg bg-base-300  p-1 border-2 border-base"
             />
           </div>
