9e0e6cfb013ccbff41775ade09a66384f11a79b9
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-20 12:26:48 -0500

    Add framer-motion animation to page components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+import { motion } from 'framer-motion';
 import Heading from '@/components/ui/Heading';
 import SubHeading from '@/components/ui/SubHeading';
 import Image from 'next/image';
@@ -6,13 +8,24 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className="relative">
-      <Image src="/movie.jpg" alt="Movie" layout="fill" objectFit="cover" className="absolute z-0" />
+      <Image
+        src="/movie.jpg"
+        alt="Movie"
+        layout="fill"
+        objectFit="cover"
+        className="absolute z-0"
+      />
       <div className="relative z-10 container mx-auto mt-1 p-4">
         <div className="text-3xl text-center mb-2 mt-3 flex">
           <Link href={'/movies'}></Link>
         </div>
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-          <div className="relative z-10 flex flex-col justify-center items-center bg-base-300 p-4 rounded-lg shadow-lg card-title hover:underline">
+          <motion.div
+            className="relative z-10 flex flex-col justify-center items-center bg-base-300 p-4 rounded-lg shadow-lg card-title hover:underline"
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5 }}
+          >
             <Link href={'/movies'}>
               <SubHeading
                 title="Search your favorite actor"
@@ -28,8 +41,13 @@ export default function Home() {
                 className="object-cover rounded-lg shadow-lg bg-base-300  p-1 border-2 border-base"
               />
             </div>
-          </div>
-          <div className="relative z-10 flex flex-col justify-center items-center bg-base-300 p-4 rounded-lg shadow-lg card">
+          </motion.div>
+          <motion.div
+            className="relative z-10 flex flex-col justify-center items-center bg-base-300 p-4 rounded-lg shadow-lg card"
+            initial={{ opacity: 0, x: 100 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5 }}
+          >
             <div className="card-title hover:underline">
               {' '}
               <Link href={'/movies'}>
@@ -48,7 +66,7 @@ export default function Home() {
                 className="object-cover rounded-lg shadow-lg bg-base-300  p-1 border-2 border-base"
               />
             </div>
-          </div>
+          </motion.div>
         </div>
       </div>
     </div>
