f94e78595c6f17d41447a5293e4e72afd8e6f30f
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-21 02:30:11 -0500

    Refactor about page layout and content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -16,41 +16,79 @@ export default function page() {
           {' '}
           <Heading title="About" iconClass="fas fa-info" />
         </div> */}
-        <div className="flex items-center justify-center bg-base-300 w-1/5 mx-auto mb-4">
+        <div className="flex items-center justify-center bg-base-300 w-1/5 mx-auto mb-4 rounded glass">
           <SubHeading title="about FLixer" iconClass="fas fa-film" />
-          
         </div>
-        <div className="container gap-5 grid grid-cols-1 md:grid-cols-2 items-center justify-center mx-auto">
+        <div className="container gap-5 grid grid-cols-1 md:grid-cols-2 items-center justify-center mx-auto mb-4">
           <div className="card items-center border-2 rounded-md shadow-lg bg-base-300">
             <div className="card-title">
-              <SubHeading title="card title" iconClass="fas fa-info-circle" />
+              <SubHeading
+                title="what is flixer..?"
+                iconClass="fas fa-info-circle"
+              />
             </div>
             <div className="card-body">
-              <p>content 1</p>
+              <ul>
+                <li>
+                  <i className="fas fa-check p-2"></i>
+                  The flixer app lets you search the vast imdb database to
+                  discover info on your favorite tv shows and movies
+                </li>
+                <li>
+                  <i className="fas fa-check p-2"></i>
+                  Search by your favorite actor, movie or TV show
+                </li>
+              </ul>
             </div>
           </div>
           <div className="card items-center border-2 rounded-md shadow-lg bg-base-300">
             <div className="card-title">
-              <SubHeading title="card title" iconClass="fas fa-info-circle" />
+              <SubHeading
+                title="upcoming features"
+                iconClass="fas fa-info-circle"
+              />
             </div>
             <div className="card-body">
-              <p>card body</p>
+              <ul>
+                <li>
+                  <i className="fas fa-check p-2"></i>
+                  Additional info for each result using dynamic routes (for the
+                  nerds). Whatever will enhance the user experience will be
+                  implement.
+                </li>
+                <li>
+                  <i className="fas fa-check p-2"></i>
+                  An add to favorite lists seems like the next logical feature
+                  after that.
+                </li>
+              </ul>
             </div>
           </div>
           <div className="card items-center border-2 rounded-md shadow-lg bg-base-300">
             <div className="card-title">
-              <SubHeading title="card title" iconClass="fas fa-info-circle" />
+              <SubHeading title="Feedback" iconClass="fas fa-info-circle" />
             </div>
             <div className="card-body">
-              <p>card body</p>
+              <p>Please provide feedback on the your user experience.</p>
+              <br />
+              <p>Your feedback is what I draw on to make improvements 😊</p>
             </div>
           </div>
           <div className="card items-center border-2 rounded-md shadow-lg bg-base-300">
             <div className="card-title">
-              <SubHeading title="card title" iconClass="fas fa-info-circle" />
+              <SubHeading
+                title="Upcoming versions"
+                iconClass="fas fa-info-circle"
+              />
             </div>
             <div className="card-body">
-              <p>card body</p>
+              <p>
+                As changes are made the user interface will change over time.
+              </p>
+              <br/>
+              <p>
+                I will be adding additional features to the app as time goes on.
+              </p>
             </div>
           </div>
         </div>
