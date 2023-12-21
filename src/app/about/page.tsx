a901d7e559f0bc7056ef4b3287a519f4010779fc
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-21 01:31:12 -0500

    Refactor about page styling and add content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,15 +4,26 @@ import SubHeading from '@/components/ui/SubHeading';
 
 export default function page() {
   return (
-    <div style={{ backgroundImage: `url(/movie.jpg)`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}>
+    <div
+      style={{
+        backgroundImage: `url(/movie.jpg)`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
       <div className="min-h-screen ">
-        <div className='p-1 border-1 bg-base-300 rounded-md flex-col my-5 w-1/5 mt-5 mx-auto'>          <Heading title="About" iconClass="fas fa-info" />
-          <SubHeading title="FLix Finder" iconClass="fas fa-film" />
+        {/* <div className=" border-1 bg-base-300 rounded-md flex-col my-8 w-1/5  mx-auto">
+          {' '}
+          <Heading title="About" iconClass="fas fa-info" />
+        </div> */}
+        <div className="flex items-center justify-center bg-base-300 w-1/5 mx-auto mb-4">
+          <SubHeading title="about FLixer" iconClass="fas fa-film" />
+          
         </div>
         <div className="container gap-5 grid grid-cols-1 md:grid-cols-2 items-center justify-center mx-auto">
           <div className="card items-center border-2 rounded-md shadow-lg bg-base-300">
             <div className="card-title">
-              <SubHeading title='card title' iconClass='fas fa-info-circle'/>
+              <SubHeading title="card title" iconClass="fas fa-info-circle" />
             </div>
             <div className="card-body">
               <p>content 1</p>
@@ -20,7 +31,7 @@ export default function page() {
           </div>
           <div className="card items-center border-2 rounded-md shadow-lg bg-base-300">
             <div className="card-title">
-              <SubHeading title='card title' iconClass='fas fa-info-circle'/>
+              <SubHeading title="card title" iconClass="fas fa-info-circle" />
             </div>
             <div className="card-body">
               <p>card body</p>
@@ -28,7 +39,7 @@ export default function page() {
           </div>
           <div className="card items-center border-2 rounded-md shadow-lg bg-base-300">
             <div className="card-title">
-              <SubHeading title='card title' iconClass='fas fa-info-circle'/>
+              <SubHeading title="card title" iconClass="fas fa-info-circle" />
             </div>
             <div className="card-body">
               <p>card body</p>
@@ -36,7 +47,7 @@ export default function page() {
           </div>
           <div className="card items-center border-2 rounded-md shadow-lg bg-base-300">
             <div className="card-title">
-              <SubHeading title='card title' iconClass='fas fa-info-circle'/>
+              <SubHeading title="card title" iconClass="fas fa-info-circle" />
             </div>
             <div className="card-body">
               <p>card body</p>
